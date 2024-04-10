31eabbcc74b775cb01357d4c09d71c586a6c0bcf
SARA JUSTINO DA SILVA <sara.jsilva1@senacsp.edu.br>
2024-04-09 21:04:10 -0300

    commitTeste

--- FoxBrinquedos/resources/views/TelaLogin.blade.php
+++ FoxBrinquedos/resources/views/TelaLogin.blade.php
@@ -32,14 +32,7 @@
             width: 100px;
             
         }
-        form input[type="text"],
-        form input[type="password"] {
-            display: block;
-            width: 50%;
-            margin-bottom: 5px;
-            background-color: #ffffff; 
-            font-family: 'Times New Roman', Times, serif;
-        }
+
 
         
         .btn_cadastrar:link{
@@ -51,10 +44,10 @@
         }
 
         .custom-btn {
-            width: 50%;
-            background-color: #F39337;
-            border-color: #F39337;
-            color: #432075;
+            width: 400px;
+            background-color: #43ADDA;
+            border-color: #43ADDA;
+            color: #102B7B;
             font-weight: bold;
         }
         .custom-btn:hover {
@@ -70,6 +63,11 @@
             margin-top:-20px;
 
         }
+        .form-floating{
+            width: 400px;
+            padding-top: 0px; 
+            padding-bottom: 0px;
+        }
 
 
       
@@ -83,11 +81,17 @@
         
        
        <div class="logo_login">
-            <img src="{{asset('logo2.png')}}" alt="logo" class="img_logo">
+            <img src="{{asset('logo.png')}}" alt="logo" class="img_logo">
         </div>
         <h4>Login</h4>
-        <input class="form-control" type="text" placeholder="Email:" aria-label="default input example">
-        <input type="password" placeholder="Senha:" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+        <div class="form-floating mb-2">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="Email">
+                    <label for="floatingInput">Email:</label>
+                </div>
+        <div class="form-floating mb-2">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Senha">
+                    <label for="floatingPassword">Senha:</label>
+                </div>
 
         <button class="btn btn-primary custom-btn" type="button">Entrar</button>
 
