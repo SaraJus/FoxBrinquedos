421cd17fdddcece83d5de2c90c6382da548bda72
Sara Justino <sara.sjustino@gmail.com>
2024-04-21 01:50:23 -0300

    commit sara

--- FoxBrinquedos/app/Http/Controllers/ProdutoController.php
+++ FoxBrinquedos/app/Http/Controllers/ProdutoController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 use App\Models\Produto;
 
+
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
     public function produto(){
-        return view('produto')->with('produtos', Produto::all());
+        return view('produto')->with('produtos', Produto::paginate(10));
+
     }
 
-    public function show(Produto $produto){
-        return view('show')->with('produto', $produto);
+    public function show(){
+        return view('show')->with('show');
     }
+    
+
+
+
+
 }
