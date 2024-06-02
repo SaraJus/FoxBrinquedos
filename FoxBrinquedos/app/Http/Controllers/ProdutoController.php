3a04113f0f62609c7c519d65a7cf1c030cee9bae
Sara Justino <sara.sjustino@gmail.com>
2024-06-02 11:06:37 -0300

    commit sara - Tela Endereço, Finalizar, e modificação na tela Dashbord

--- FoxBrinquedos/app/Http/Controllers/ProdutoController.php
+++ FoxBrinquedos/app/Http/Controllers/ProdutoController.php
@@ -4,45 +4,53 @@ namespace App\Http\Controllers;
 use App\Models\Produto;
 use App\Models\Categoria;
 
-
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    public function produto(){
-
-        return view('produto.produto')->with('produtos', Produto::paginate(10));
-     ;
+    public function produto()
+    {
+        // Buscar apenas produtos ativos
+        $produtos = Produto::where('PRODUTO_ATIVO', true)->paginate(10);
+        return view('produto.produto')->with('produtos', $produtos);
     }
 
-    public function show(Produto $produto){
-         
-         $categoria = $produto->categoria;
+    public function show(Produto $produto)
+    {
+        $categoria = $produto->categoria;
 
-         $produtosRelacionados = Produto::where('CATEGORIA_ID', $categoria->CATEGORIA_ID)
-             ->where('PRODUTO_ID', '!=', $produto->PRODUTO_ID) 
-             ->inRandomOrder() 
-             ->take(6) 
-             ->get();
- 
-         return view('produto.show', compact('produto', 'produtosRelacionados'));
-     }
-        
+        // Buscar produtos relacionados que também estão ativos
+        $produtosRelacionados = Produto::where('CATEGORIA_ID', $categoria->CATEGORIA_ID)
+            ->where('PRODUTO_ID', '!=', $produto->PRODUTO_ID)
+            ->where('PRODUTO_ATIVO', true)
+            ->inRandomOrder()
+            ->take(6)
+            ->get();
+
+        return view('produto.show', compact('produto', 'produtosRelacionados'));
+    }
+
     public function categoria($categoria_nome)
     {
         $categoria = Categoria::where('CATEGORIA_NOME', $categoria_nome)->firstOrFail();
 
-        $produtos = Produto::where('CATEGORIA_ID', $categoria->CATEGORIA_ID)->get();
+        // Buscar apenas produtos ativos na categoria fornecida
+        $produtos = Produto::where('CATEGORIA_ID', $categoria->CATEGORIA_ID)
+            ->where('PRODUTO_ATIVO', true)
+            ->get();
 
-        return view('categoria', compact('produtos','categoria'));
+        return view('categoria', compact('produtos', 'categoria'));
     }
+
     public function pesquisar(Request $request)
     {
         $termoPesquisa = $request->input('q');
-    
-        $produtos = Produto::where('PRODUTO_NOME', 'like', '%' . $termoPesquisa . '%')->get();
-    
+
+        // Buscar apenas produtos ativos que correspondam ao termo de pesquisa
+        $produtos = Produto::where('PRODUTO_NOME', 'like', '%' . $termoPesquisa . '%')
+            ->where('PRODUTO_ATIVO', true)
+            ->get();
+
         return view('resultado_pesquisa')->with('produtos', $produtos)->with('termoPesquisa', $termoPesquisa);
     }
-    
 }
